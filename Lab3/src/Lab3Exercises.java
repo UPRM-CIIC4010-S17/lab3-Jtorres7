4600b06ff14a42d842a18d0991671f856cb17e49
4015b14 <4015b14@quenepa.ece.uprm.edu>
2017-02-08 10:19:49 -0400

    Added and centered Oval

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,8 +4,8 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Jose L. Torres");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(900, 450);
-		myFrame.setSize(200, 200);
+		myFrame.setLocation(550, 300);
+		myFrame.setSize(800, 500);
 		MyPanelClass myPanel = new MyPanelClass();
 		myFrame.getContentPane().add(myPanel);
 		myFrame.setVisible(true);
